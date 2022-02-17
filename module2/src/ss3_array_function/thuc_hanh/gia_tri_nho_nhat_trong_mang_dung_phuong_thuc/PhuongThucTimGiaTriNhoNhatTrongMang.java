c4bbfd7957c469604d62b5dd415d023ae2e03d0b
Khoa Pham <vietkhoa93@gmail.com>
2022-02-17 21:24:05 +0700

    lession method

---
+++ module2/src/ss3_array_function/thuc_hanh/gia_tri_nho_nhat_trong_mang_dung_phuong_thuc/PhuongThucTimGiaTriNhoNhatTrongMang.java
@@ -0,0 +1,21 @@
+package ss3_array_function.thuc_hanh.gia_tri_nho_nhat_trong_mang_dung_phuong_thuc;
+
+public class PhuongThucTimGiaTriNhoNhatTrongMang {
+
+    public static void main(String[] args) {
+        int[] arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minValue(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+
+    public static int minValue(int[] array) {
+        int index = 0;
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < array[index]) {
+                index = i;
+            }
+        }
+        return index;
+    }
+
+}
