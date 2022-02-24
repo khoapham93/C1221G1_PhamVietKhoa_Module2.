f447245588fdca66d160d3569b56a2f51c8d93b2
Khoa Pham <vietkhoa93@gmail.com>
2022-02-24 08:26:09 +0700

    abstract class and interface

--- module2/src/ss6_inheritance/bai_tap/triangle/TestApplication.java
+++ module2/src/ss6_inheritance/bai_tap/triangle/TestApplication.java
@@ -5,18 +5,28 @@ import java.util.Scanner;
 public class TestApplication {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        double side1 = 0;
+        double side2 = 0;
+        double side3 = 0;
+        do {
+            System.out.print("Enter the first side of triangle: ");
+            side1 = Double.parseDouble(scanner.nextLine());
+            System.out.print("Enter the second side of triangle: ");
+            side2 = Double.parseDouble(scanner.nextLine());
+            System.out.print("Enter the third side of triangle: ");
+            side3 = Double.parseDouble(scanner.nextLine());
 
-        System.out.print("Enter the first side of triangle: ");
-        double side1 = Double.parseDouble(scanner.nextLine());
-        System.out.print("Enter the second side of triangle: ");
-        double side2 = Double.parseDouble(scanner.nextLine());
-        System.out.print("Enter the third side of triangle: ");
-        double side3 = Double.parseDouble(scanner.nextLine());
+            if ((side1 + side2 > side3) && (side1 + side3 > side2) && (side3 + side2 > side1)) {
+                break;
+            } else {
+                System.out.println("3 number entered is not sides of triangle");
+            }
+        } while (true);
+
         System.out.print("Enter the color of triangle: ");
         String color = scanner.nextLine();
 
         Triangle triangle = new Triangle(color, side1, side2, side3);
         System.out.println(triangle);
-
     }
 }
---
+++ module2/src/ss7_abstract_class_interface/thuc_hanh/interface_comparator/CircleComparator.java
@@ -0,0 +1,15 @@
+package ss7_abstract_class_interface.thuc_hanh.interface_comparator;
+
+import ss6_inheritance.thuc_hanh.shape.Circle;
+
+import java.util.Comparator;
+
+public class CircleComparator implements Comparator<Circle> {
+
+    @Override
+    public int compare(Circle c1, Circle c2) {
+        if (c1.getRadius() > c2.getRadius()) return 1;
+        else if (c1.getRadius() < c2.getRadius()) return -1;
+        else return 0;
+    }
+}
